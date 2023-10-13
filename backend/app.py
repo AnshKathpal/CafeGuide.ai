81eabc552a3f9208ac670edd3c766653fb8ffa3a
AnshKathpal <storage.2.3107@gmail.com>
2023-10-14 00:04:09 +0530

    recommendations

--- backend/app.py
+++ backend/app.py
@@ -97,27 +97,53 @@ product_function = [
                 "data": {
                     "type": "array",
                     "items": {
-                        "type" : "object",
-                        "properties" : {
+                        "type": "object",
+                        "properties": {
                             "id": {
                                 "type": "number",
                                 "description": "Product ID, e.g. 1"
                             },
-                            "name" : {
-                                "type" : "string",
-                                "description" : "Cafe Name, e.g. Diggin"
+                            "name": {
+                                "type": "string",
+                                "description": "Cafe Name, e.g. Diggin"
                             },
-                            "image" : {
-                                "type" : "string",
-                                "description" : "Cafe Image, e.g. https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/i/t/p15105-145690007356d687e9547a8.jpg?tr=tr:n-xlarge"
+                            "image": {
+                                "type": "string",
+                                "description": "Cafe Image, e.g. https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/i/t/p15105-145690007356d687e9547a8.jpg?tr=tr:n-xlarge"
+                            },
+                            "recommendation": {
+                                "type": "object",
+                                "properties": {
+                                    "name": {
+                                        "type": "string",
+                                        "description": "Recommendation Name, e.g. Diggin"
+                                    },
+                                    "address": {
+                                        "type": "string",
+                                        "description": "Recommendation Address, e.g. 11, Santushti Shopping Complex, Race Course Road, Chanakyapuri, New Delhi, Delhi 110021, India"
+                                    },
+                                    "rating": {
+                                        "type": "string",
+                                        "description": "Recommendation Rating e.g. 4.5 to 5 stars"
+                                    },
+                                    "image": {
+                                        "type": "string",
+                                        "description": "Recommendation Image e.g. https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/i/t/p15105-145690007356d687e9547a8.jpg?tr=tr:n-xlarge"
+                                    },
+                                    "location": {
+                                        "type": "string",
+                                        "description": "Recommendation Location e.g. https://maps.app.goo.gl/nkcnz3f4yCTqEvVu6"
+                                    }
+                                },
+                                "required": ["name", "address", "rating", "image", "location"]
                             }
                         },
-                        "required" : ["id","name","image"]
+                        "required": ["id", "name", "image"]
                     }
                 }
             },
-            "required": ["data"]    
-        },
+            "required": ["data"]
+        }
     }
 ]
 
@@ -130,48 +156,51 @@ def chat():
         result = qa({"question": question})
         answer_from_chat = result["answer"]
 
-
         chatReply = format_links_as_html(answer_from_chat)
-
 
         docs = vectordb.similarity_search(question, k=10)
         # print(docs)
 
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0613",
-            messages = [{"role" : "user", "content" : f"""{docs}"""}],
-            functions = product_function,
-            function_call = "auto"
+            messages=[{"role": "user", "content": f"""{docs}"""}],
+            functions=product_function,
+            function_call="auto"
         )
 
         output = completion.choices[0].message
-        print(output, "output")
+        # print(output, "output")
 
         arguments = json.loads(output.function_call.arguments)
 
         print(arguments, "args")
+
         ids = [item['id'] for item in arguments['data']]
         print(ids)
         names = [item["name"] for item in arguments["data"]]
         print(names)
         images = [item["image"] for item in arguments["data"]]
         print(images)
+        recommendations = [item["recommendation"]
+                           for item in arguments["data"]]
+        print(recommendations)
 
         response_data = {
             "answer_from_chat": chatReply,
-            "args" : ids,
-            "name" : names,
-            # "image" : images
+            "args": ids,
+            "name": names,
+            "recommended": recommendations
         }
 
         if "images" in question.lower():
             response_data["image"] = images
-            
+
         return jsonify(response_data)
     except Exception as e:
         error_message = str(e)
         return jsonify({"error": error_message}), 500
-    
+
+
 def format_links_as_html(text):
     # This function formats links as HTML anchor tags
     words = text.split()
@@ -179,7 +208,8 @@ def format_links_as_html(text):
     for word in words:
         if word.startswith("http://") or word.startswith("https://"):
             # If it's a link, format it as an HTML anchor tag
-            formatted_text.append(f'<a href="{word}" target="_blank">{word}</a>')
+            formatted_text.append(
+                f'<a href="{word}" target="_blank">{word}</a>')
         else:
             formatted_text.append(word)
     return " ".join(formatted_text)
