26bfb8c1d6657fcd569704d114653027fea97cab
AnshKathpal <storage.2.3107@gmail.com>
2023-10-19 02:54:41 +0530

    updated

--- backend/app.py
+++ backend/app.py
@@ -62,7 +62,6 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm, chain_type="stuff", retriever=vectordb.as_retriever())
-# print(qa.run("Google map location of Hauz Khas Social"))
 
 memory = ConversationBufferMemory(
     memory_key="chat_history",
