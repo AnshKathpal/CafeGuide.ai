06c7450a669ec4601bbd71a371f87affd9cdb3d4
AnshKathpal <storage.2.3107@gmail.com>
2023-10-21 16:12:23 +0530

    sqlite added

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv, find_dotenv
 import openai
