4ad55133b26f6127ec2fa07c8a90f4e343ca3936
AnshKathpal <storage.2.3107@gmail.com>
2023-11-23 01:52:51 +0530

    cafe-backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 from flask import Flask, request, jsonify
